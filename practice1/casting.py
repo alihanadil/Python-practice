e77d92fe71dc46dcedc80891742fef9617424afd
Alihanadil <adilhanov0805@gmail.com>
2026-01-25 20:18:07 +0500

    Added missing files

--- practice1/casting.py
+++ practice1/casting.py
@@ -2,6 +2,8 @@
 x = 5
 y = 6.4
 z = 4.3
+w = "4.2"
 print(float(x)) #will turn int 5 into float 5.0
 print(int(y)) #6.4 will round up to 6
 print(int(z+x)) #equals 9.3 but rounds up to 9
+print (float(w)) # string will become a float 4.2
---
+++ practice1/numbers.py
@@ -0,0 +1,20 @@
+# int
+x = 1
+y = 35656222554887711
+    
+print(type(x))
+print(type(y))
+
+# float
+z = -35.59
+y = 12E4
+
+print(type(x))
+print(type(y))
+ 
+# complex
+x = 3+5j
+y = 5j
+
+print(type(x))
+print(type(y))
